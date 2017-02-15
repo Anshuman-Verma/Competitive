a54a83366ce2e52632735e9164acbeaa7834f7b2
Anshuman Verma <anshu.av97@gmail.com>
2017-02-15 20:08:05 +0530

    Updates

--- Chef/FEB-L/GERMANDE.cpp
+++ Chef/FEB-L/GERMANDE.cpp
@@ -13,29 +13,40 @@ int main(){
 		int o1,o2,i;
 		cin >> o1 >> o2;
 		const int size = 2*o1*o2;
-		
-		int r[size];
-		for(i = 0; i<size/2; i++){
-			cin >> r[i];
+	
+		vector<bool> r(size);
+		vector<int> sum(size);
+		 for(i = 0; i<size/2; i++){
+			int temp;
+			cin >> temp;
+			r[i] = temp;
 			r[(size)/2+i] = r[i];
-		}
+		}  
 		
 	// --END OF INPUTS--
 	
-		int flag = 0,start = 0;
+		int maxOnes=0,start = 0;
 		
-		for(int j = 0; j<(size)/2; j++){
-			if(r[j]==1)
-				ones++;
+		while(start < o2){
+			
+			int onesRep=0;
+			for(int i = start; i < size/2; i+=o2){
+				int Ones = 0;
+				
+				//Ones = count(r.begin()+i, r.begin()+i+o2, true);  //  causes TLE
+				
+				if(Ones>o2/2)
+					onesRep++;
+			
+			}
+			if(onesRep>maxOnes)
+				maxOnes = onesRep;
+			
+		start++;
 		}
-	
-	
-	
-	
 		
-	}
-	
-		
+		cout << ((maxOnes > o1/2) ? "1" : "0") << "\n";
+
 		
 	}
 	
